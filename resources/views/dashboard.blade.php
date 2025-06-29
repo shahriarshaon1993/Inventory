892f084e5787cd9a3fbbf020f700863bb20b7b7c
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-06-30 02:25:25 +0600

    feat: modify dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,99 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <nav class="flex items-center justify-between pb-1 mb-3">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <li class="inline-flex items-center">
+                <a href="" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 gap-2">
+                    <x-icon.home class="w-4 h-4" />
+                    Dashboard
+                </a>
+            </li>
+        </ol>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+        <div class="flex"></div>
+    </nav>
+
+    <div class="overflow-auto">
+        <table class="min-w-full">
+            <thead class="bg-gray-100 border-b">
+                <tr>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        #
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Name
+                    </th>
+                    <th v-if="search" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Path
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Owner
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Last Modified
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Size
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr class="border-b transition duration-300 ease-in-out hover:bg-blue-100 cursor-pointer">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 text-yellow-500">
+                        1
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 flex items-center">
+                        asdasd
+                    </td>
+                    <td v-if="search" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        asdasd
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        adasdas
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        asdasd
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        asdasd
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    {{-- Dashboard --}}
+    <div class="grid grid-cols-4 gap-4 mb-4 mt-4">
+        <div class="flex items-center justify-center h-24 rounded-sm bg-gray-50">
+            <p class="text-2xl text-gray-400">
+                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                </svg>
+            </p>
+        </div>
+
+        <div class="flex items-center justify-center h-24 rounded-sm bg-gray-50">
+            <p class="text-2xl text-gray-400">
+                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                </svg>
+            </p>
+        </div>
+
+        <div class="flex items-center justify-center h-24 rounded-sm bg-gray-50">
+            <p class="text-2xl text-gray-400">
+                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                </svg>
+            </p>
+        </div>
+
+        <div class="flex items-center justify-center h-24 rounded-sm bg-gray-50">
+            <p class="text-2xl text-gray-400">
+                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                </svg>
+            </p>
         </div>
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,67 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="h-screen bg-gray-50 flex w-full gap-4">
+            <div class="min-w-[200px]">
+                <a href="{{ route('dashboard') }}" class="h-[80px] px-3 flex items-center gap-3">
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    Inventory
+                </a>
+
+                <div class="px-3">
+                    <div class="py-3">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            Dashboard
+                        </x-nav-link>
+                    </div>
+                </div>
+            </div>
+
+            <main class="flex flex-col flex-1 p-4 overflow-hidden">
+                <div class="flex items-center justify-between w-full mb-3">
+                    <div></div>
+                    <!-- Settings Dropdown -->
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                </div>
+                <div class="flex-1 flex flex-col overflow-hidden">
+                    {{ $slot }}
+                </div>
+            </main>
+        </div>
+
+
+        {{-- <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -31,6 +91,6 @@
             <main>
                 {{ $slot }}
             </main>
-        </div>
+        </div> --}}
     </body>
 </html>
