db1d770ba39ca2f753a0144c7aca46358266591d
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-06-30 11:16:21 +0600

    fix(product): set fillable property DOC and add annotation id

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,16 +6,22 @@ use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
  * @property string $name
  * @property string $purchase_price
  * @property string $sell_price
- * @property integer $opening_stock
- * @property integer $current_stock
+ * @property int $opening_stock
+ * @property int $current_stock
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
 class Product extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
         'name',
         'purchase_price',
