4e321979bb5cb2e1a456acf4e6080f41a09db7a4
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-06-30 11:49:41 +0600

    feat(sale): implement sale model and migration

---
+++ app/Models/Sale.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $discount
+ * @property string $vat
+ * @property string $subtotal
+ * @property string $total_amount
+ * @property string $paid_amount
+ * @property string $due_amount
+ * @property Carbon|null $date
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
+class Sale extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'vat',
+        'date',
+        'discount',
+        'subtotal',
+        'total_amount',
+        'paid_amount',
+        'due_amount',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+        ];
+    }
+}
