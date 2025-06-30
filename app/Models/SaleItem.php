b688741c11ae1ee502fae19e4d6427b764adc9af
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-06-30 12:04:26 +0600

    feat(sale items): implement sale model and migration

---
+++ app/Models/SaleItem.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $sale_id
+ * @property int $product_id
+ * @property int $quantity
+ * @property string $unit_price
+ * @property string $total
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
+class SaleItem extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+        'total',
+    ];
+
+    /**
+     * Sale items belongs to product
+     * 
+     * @return BelongsTo<Product, SaleItem>
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * Sale items belongs to sale
+     * 
+     * @return BelongsTo<Sale, SaleItem>
+     */
+    public function sale(): BelongsTo
+    {
+        return $this->belongsTo(Sale::class);
+    }
+}
